a849b8ab398f2f2daccfe51866d36d267638b788
arran lyon <arran.lyon@hotmail.com>
2023-08-28 00:04:23 +0200

    tidying HOTSPOT folder

---
+++ HOTSPOT/SERVER/deployment/picamera2_test.py
@@ -0,0 +1,12 @@
+import time
+
+from picamera2 import Picamera2, Preview
+
+picam2 = Picamera2()
+picam2.start_preview(Preview.QTGL, x=100, y=200)
+
+preview_config = picam2.create_preview_configuration()
+picam2.configure(preview_config)
+
+picam2.start()
+time.sleep(2)
