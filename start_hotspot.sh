6b68f1ecd811047114223245b5acc5b9283be0fa
arran lyon <arran.lyon@hotmail.com>
2023-09-05 11:48:59 +0200

    startup files

--- start_hotspot.sh
+++ start_hotspot.sh
@@ -3,18 +3,18 @@
 echo "*** Starting HOTSPOT ***"
 echo
 echo "*** Beginning Server ***"
+echo
 # cd /home/hotspot/Hacking_Heuristics/HOTSPOT/SERVER/deployment
 cd /home/hotspot/HackingHeuristics/HOTSPOT/SERVER/deployment 
-python server_new.py --load --trace &
+/usr/bin/python3 server_new.py --load --trace &
 SVR=$!
 
 echo "*** Starting RD ***"
+echo
 /home/hotspot/openframeworks/apps/myApps/rd_server/bin/rd_server
 # /home/arran/Projects/OF/apps/myApps/rd_server/bin/rd_server 
 
 echo "*** Killing Server ***"
 kill $SVR
+echo
 echo "*** HOTSPOT Closed ***"
-
-
-
