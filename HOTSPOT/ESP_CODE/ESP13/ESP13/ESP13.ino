6a2fcee39e3de6a95b6c67402b0521f8e7fe1aa6
arran lyon <arran.lyon@hotmail.com>
2023-09-07 08:33:58 +0200

    fixed ESP13 i2c address

--- HOTSPOT/ESP_CODE/ESP13/ESP13/ESP13.ino
+++ HOTSPOT/ESP_CODE/ESP13/ESP13/ESP13.ino
@@ -245,7 +245,7 @@ void setup() {
   }
 
   // init screen...
-  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3D)) { // Address 0x3D for 128x64
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3D for 128x64
     Serial.println(F("SSD1306 allocation failed"));
     for(;;);
   }
